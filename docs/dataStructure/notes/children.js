09321b2fe9a85784f47d5a235a7cfe712482cf66
v_vzhwzhang <v_vzhwzhang@tencent.com>
2021-08-02 14:42:02 +0800

    commit

---
+++ docs/dataStructure/notes/children.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        title: "数组和栈",
+        collapsable: true,
+        children: ["/dataStructure/notes/01_数组和栈"],
+    },
+    {
+        title: "基础案例篇",
+        collapsable: true,
+        children: ["/dataStructure/notes/02_基础案例篇"],
+    },
+]
--- docs/dataStructure/slidbar.js
+++ docs/dataStructure/slidbar.js
@@ -1,12 +1,7 @@
 module.exports = [
     {
-        title: '入门篇',
+        title: '数组栈',
         collapsable: true,
-        children: ['/dataStructure/notes/01_入门']
-    },
-    {
-        title: '基础案例篇',
-        collapsable: false,
-        children: ['/dataStructure/notes/02_基础案例篇']
-    },
+        children: require('./notes/children')
+    }
 ]
